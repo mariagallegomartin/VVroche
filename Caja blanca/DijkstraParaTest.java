36247a5c07ff908370049d4c9e069561e5e947ea
miguelangellopezr <37208260+miguelangellopezr@users.noreply.github.com>
2019-12-11 10:05:50 +0100

    Caja Blanca
    Diagrama
    Dijstra con variaciones en funcion get path para test
    Codigo Junit

---
+++ Caja blanca/DijkstraParaTest.java
@@ -0,0 +1,185 @@
+import java.util.ArrayList;
+
+/**
+ * Class implementing Dijkstra's shortest path algorithm
+ * for directed simple (i.e. without loops) graphs
+ * with non-negative weighted edges.
+ * 
+ * For more info: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm 
+ */
+public class Dijkstra {
+	private Double[][] adjMatrix;			//Adjacency matrix
+	
+	private Double[] distances;				//Vector of estimated distances
+	private boolean[] visited;				//Vector of visited nodes
+	private Integer[] prev;					//Previous node in the shortest path
+	private Integer nVertices;				//Number of vertices of the graph
+	
+	private boolean error = false;			//Flag: if an error happened
+	private boolean dijkstraExec;			//Flag: if the algorithm was executed
+	
+	protected String line;
+	
+	/**
+	 * Constructor of the Dijkstra class.
+	 * This function saves the adjacency matrix of a 
+	 * directed simple graph with weighted edges.
+	 * 
+	 * The nodes are labeled by their position in the
+	 * adjacency matrix. That is, the vertex whose
+	 * outgoing edges are saved in the ith row of adjMatrix
+	 * is the vertex i.
+	 * 
+	 * @param adjMatrix Square matrix that represents
+	 * the adjacency matrix of the graph. The entry (i,j)
+	 * of adjMatrix represents the weight of the edge that
+	 * goes from vertex i to vertex j. The weights must be
+	 * positive. A negative entry in the position (i,j)
+	 * means that there is no edge between the vertices i and j.
+	 *  
+	 * @param nVertices Non-negative number of vertices of the graph,
+	 * that is, the order of the adjacency matrix. 
+	 */
+	public Dijkstra(Double[][] adjMatrix, Integer nVertices){
+		this.adjMatrix = adjMatrix;
+		this.nVertices = nVertices;
+	}
+	
+	/**
+	 * Private function that initializes all the data
+	 * structures as required by the Dijkstra's algorithm.
+	 */
+	private void initializeDataStructures() {
+		// Creates the data structures
+		distances = new Double[nVertices];
+		visited = new boolean[nVertices];
+		prev = new Integer[nVertices];
+		
+		// Initialization of data structures
+		for(int i=0; i<nVertices; i++) {
+			distances[i] = Double.POSITIVE_INFINITY;	//The estimated distance is infinity
+			visited[i] = false;							//No node has been visited
+			prev[i] = -1;								//No shortest path
+		}
+		
+		return;
+	}
+	
+	/**
+	 * Returns the next vertex to be analyzed by the
+	 * Dijkstra algorithm. As required, that should
+	 * be the unvisited vertex with smallest expected cost.
+	 * 
+	 * @return Next vertex to be explored.
+	 */
+	private Integer nextCur() {
+		Integer next = -1;
+		Double cost = Double.POSITIVE_INFINITY;
+		
+		for(int i=0; i<nVertices; i++) {
+			if(!visited[i] && distances[i]<cost) {
+				next = i;
+				cost = distances[i];
+			}
+		}
+		
+		return next;
+	}
+	
+	/**
+	 * Main call of Dijkstra's shortest path algorithm.
+	 * Given a directed simple graph with non-negative
+	 * weighted edges, an initial vertex ini and a
+	 * final vertex end, this function computes the
+	 * shortest path between ini and end. 
+	 * 
+	 * The minimum distance is returned by the function.
+	 * The list of nodes that form the shortest path
+	 * can be recovered by calling the function getPath
+	 * after the call to this function.
+	 * 
+	 * If there is no path between ini and end, the function
+	 * returns Double.POSITIVE_INFINITY and no shortest path
+	 * is computed.
+	 * 
+	 * @param ini Initial vertex
+	 * @param end Final vertex
+	 * @return Cost of the shortest path between ini and end.
+	 */
+	public Double computeShortestPath(Integer ini, Integer end) {
+		Double newDistance;
+		initializeDataStructures();
+		
+		Integer cur = ini;	//Starts in the initial vertex
+		distances[ini]=0.0;	//The cost of the path from ini to itself is 0
+		while(!visited[end]) {
+			//For every vertex in the node
+			for(int neigh=0; neigh<nVertices; neigh++) {
+				//If the vertex is connected with the current node and
+				//has not been visited
+				if(adjMatrix[cur][neigh]>0 && !visited[neigh]) {
+					//Computes the distance of going to neigh via cur.
+					newDistance = distances[cur] + adjMatrix[cur][neigh];
+					
+					//If the new distance is better, update it.
+					if(distances[neigh] > newDistance) {
+						distances[neigh] = newDistance;
+						prev[neigh] = cur;
+					}
+				}
+			}
+			
+			visited[cur] = true; 	//Mark the current node as visited
+			cur = nextCur(); 		//Choose a new current node
+		}
+		
+		dijkstraExec = true;	//Dijkstra has been executed
+		return distances[end];
+	}
+	
+	/**
+	 * Get the shortest path computed by Dijkstra's algorithm.
+	 * Returns an ArrayList with the ordered list of nodes that
+	 * form the path, from the initial vertex to the final vertex.
+	 * 
+	 * If there are two possible paths with minimum cost, this
+	 * function returns the one with less number of edges.
+	 * 
+	 * If Dijkstra's algorithm has not been previously executed,
+	 * the function returns null and sets that an error occurred.
+	 * 
+	 * @param ini Initial vertex of Dijkstra's algorithm
+	 * @param end Final vertex of Dijkstra's algorithm
+	 * @return ArrayList with the shortest path.
+	 */
+	public ArrayList<Integer> getPath(Integer ini, Integer end) {
+		line = "";
+		line += "155"; if(!dijkstraExec) { 
+			error = true; line += "156";
+			line += "157"; return null; 
+		}
+		
+		ArrayList<Integer> path = new ArrayList<Integer>(); line += "160";
+		
+		//Runs over the path from end to ini by
+		//jumping through the previous nodes.
+		Integer cur = end; line += "164";
+		path.add(cur); line += "165";
+		line += "166"; while(cur != ini) { 
+			path.add(prev[cur]); line += "167";
+			cur = prev[cur]; line += "168";
+		}
+		
+		line += "171"; return path; 
+	}
+	
+	/**
+	 * Checks if an error has occurred in the
+	 * execution of the class.
+	 * 
+	 * @return True if an error happened, false if not.
+	 */
+	public boolean hasErrorHappened() {
+		return error;
+	}
+}
---
+++ Caja blanca/DijkstraTest.java
@@ -0,0 +1,71 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DijkstraTest {
+	
+	static Dijkstra dijkstra,dijkstra3;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		/*Path 1 y 2*/
+				Double [][] mpath = new Double[3][3];
+				for(int i=0; i<3;i++) {
+					for(int j=0; j<3;j++)
+						mpath[i][j]= Double.POSITIVE_INFINITY;
+				}
+				mpath[0][1]=2.0;mpath[1][2]=1.0;mpath[0][2]=5.0;
+				Integer numV = new Integer(3);
+				dijkstra = new Dijkstra(mpath,numV);
+				dijkstra.computeShortestPath(0, 2);
+		//Path 3
+				Double [][] mpath3 = new Double[1][1];
+				mpath3[0][0]= Double.POSITIVE_INFINITY;
+				Integer numV3 = new Integer(1);
+				dijkstra3 = new Dijkstra(mpath3,numV3);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetPath1() {
+		/*PATH1:
+		 *Es un grafo ya ejecutado por lo que en el primero rombo va a la izquierda
+		 *pero que no entra en el bucle while*/
+		dijkstra.getPath(0, 0);
+		assertEquals("155160164165166171", dijkstra.line);
+	}
+	
+	@Test
+	void testGetPath2() {
+		/*PATH2:
+		 *Es un grafo ya ejecutado por lo que en el primero rombo va a la izquierda
+		 *pero que entra en el bucle while*/
+		dijkstra.getPath(0, 2);
+		assertEquals("155160164165166167168167168171", dijkstra.line);
+	}
+	
+	@Test
+	void testGetPath3() {
+		/*PATH3:
+		 *Es un grafo no ejecutado por lo que en el primer rombo va a la derecha
+		 *y ahí finaliza*/
+		dijkstra3.getPath(new Integer(0), new Integer(0));
+		assertEquals("155156157", dijkstra3.line);
+	}
+
+}
